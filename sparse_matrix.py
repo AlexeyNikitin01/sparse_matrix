850c424a9168dc2aec4133e0e9ef489195c468f5
Alex <lyosha-2001@mail.ru>
2021-10-16 13:04:46 +0300

    Correct staticmethod from_file and __init__, write test for from_file

--- sparse_matrix.py
+++ sparse_matrix.py
@@ -1,13 +1,11 @@
 class SparseMatrix:
-    PATH_TO_FILE = 'MATRIX.txt'
-    PATH_TO_FILE_TWO = 'MATRIX_TWO.txt'
 
-    def __init__(self, val, cols, rows_pointers, size_mat_col, size_mat_row):
+    def __init__(self, val, cols, rows_pointers, n_cols, n_rows):
         self._val = val
         self._cols = cols
         self._rows_pointers = rows_pointers
-        self.size_mat_col = size_mat_col
-        self.size_mat_row = size_mat_row
+        self._n_cols = n_cols
+        self._n_rows = n_rows
 
     def __str__(self):
         pass
@@ -16,12 +14,8 @@ class SparseMatrix:
         pass
 
     def is_sparse(self) -> bool:
-        # return sqrt(number_rows * number_cols) > len(self.values)
+        # return sqrt(self.size_mat_row * self.size_mat_col) > len(self.values)
         pass
-
-    def matrix_add(self): # -> SparseMatrix
-        # сложение двух матриц
-        return
 
     def to_matrix(self) -> list[list[float]]:
         # Создает обычную матрицу из разреженной
@@ -30,30 +24,27 @@ class SparseMatrix:
     @staticmethod
     def from_file(path_to_file):
         with open(path_to_file) as f:
-            size_mat_col = 0
-            size_mat_row = 0
-            _val = []
-            _cols = []
-            _rows_pointers = [0, ]
+            n_rows, n_cols = map(lambda x: int(x), f.readline().split(' '))
+            val = []
+            cols = []
+            rows_pointers = [0, ]
             pointer = 0
             for line in f:
                 row = [int(x) for x in line.strip().split()]
-                size_mat_col += 1
-                for count, val_in_line in enumerate(row):
-                    if val_in_line:
-                        _val.append(val_in_line)
-                        _cols.append(count)
+                for col in range(n_cols):
+                    if row[col]:
+                        val.append(row[col])
+                        cols.append(col)
                         pointer += 1
-                _rows_pointers.append(pointer)
-            size_mat_row += max(_cols)+1
-        return SparseMatrix(_val, _cols, _rows_pointers, size_mat_col, size_mat_row)
+                rows_pointers.append(pointer)
+        return SparseMatrix(val, cols, rows_pointers, n_cols, n_rows)
 
     @staticmethod
-    def from_matrix(matrix): # -> SparseMatrix
+    def from_matrix(matrix):  # -> SparseMatrix
         # Создает разреженную матрицу из обычной
         pass
 
     @staticmethod
-    def identity(n): # -> SparseMatrix
+    def identity(n):  # -> SparseMatrix
         # Создает единичную матрицу NxN
         pass
--- test_sparse_matrix.py
+++ test_sparse_matrix.py
@@ -4,24 +4,15 @@ from sparse_matrix import SparseMatrix
 
 
 class MyTestCase(unittest.TestCase):
-    def test_increase_on_vector(self):
-        check = SparseMatrix()
-        increase_on_vector = check.increase_on_vector([1, 2, 3, 1])
-        self.assertEqual(increase_on_vector, [4, 1, 4, 2])
 
-    def test_matrix_add(self):
-        check = SparseMatrix()
-        second_matrix = [[0, 0, 1, 1],
-                         [1, 0, 0, 0],
-                         [1, 0, 1, 0],
-                         [0, 1, 0, 0]]
-        add_matrix = check.matrix_add(second_matrix)
-        result_matrix = [[0, 0, 2, 2],
-                         [2, 0, 0, 0],
-                         [2, 0, 2, 0],
-                         [0, 2, 0, 0]]
-
-        return self.assertEqual(add_matrix, result_matrix)
+    def test_from_file(self):
+        check = SparseMatrix
+        from_file = check.from_file('MATRIX.txt')
+        self.assertEqual(from_file._val, [1, 1, 1, 1, 3, 1])
+        self.assertEqual(from_file._cols, [2, 3, 0, 0, 2, 1])
+        self.assertEqual(from_file._rows_pointers, [0, 2, 3, 5, 6])
+        self.assertEqual(from_file._n_cols, 4)
+        self.assertEqual(from_file._n_rows, 4)
 
 
 if __name__ == '__main__':
